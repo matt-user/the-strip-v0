3e16b593a7d362a9d5c5478bc1e1340e533b5ea4
AurelienFT <aurefook@gmail.com>
2024-12-17 16:09:33 +0100

    Add deployment game and liquidity pool

--- scripts/src/deploy_liquidity_pool.ts
+++ scripts/src/deploy_liquidity_pool.ts
@@ -1,5 +1,5 @@
 import { Provider, Wallet } from 'fuels';
-import { UsdsFactory } from './types/usds/UsdsFactory';
+import { LiquidityPoolFactory } from './types/liquidity_pool/LiquidityPoolFactory';
 
 import * as dotenv from 'dotenv'
 dotenv.config()
@@ -8,13 +8,13 @@ const provider = await Provider.create(
     'https://testnet.fuel.network/v1/graphql'
   );
 const wallet = Wallet.fromPrivateKey(process.env.PRIVATE_KEY!, provider);
-const deploy = await UsdsFactory.deploy(wallet);
+const deploy = await LiquidityPoolFactory.deploy(wallet);
 const { contract } = await deploy.waitForResult();
 const addressInput = { bits: wallet.address.toB256() };
 const addressIdentityInput = { Address: addressInput };
 
-const { waitForResult } = await contract.functions.constructor(addressIdentityInput).call();
+const { waitForResult } = await contract.functions.initialize(addressIdentityInput).call();
 
 const { value } = await waitForResult();
 
-console.log(`USDS contract deployed at ${contract.id}`);
+console.log(`Liquidity contract deployed at ${contract.id}`);
