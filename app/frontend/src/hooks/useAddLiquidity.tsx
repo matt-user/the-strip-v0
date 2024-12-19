7aad488092623ebfc74342f19dcab4b70688d2cc
Matt Auer <mattauer@umich.edu>
2024-12-19 00:38:38 -0500

    add liquidity hook and initialize in config

---
+++ app/frontend/src/hooks/useAddLiquidity.tsx
@@ -0,0 +1,41 @@
+import { useWallet } from "@fuels/react";
+import { useMutation } from "@tanstack/react-query";
+
+import { usdsAssetId } from "@/utils/assetId";
+import { useNotification } from "./useNotification";
+import { LiquidityPool } from "@/types";
+import { liquidityPoolContractAddress } from "../../../lib";
+
+export const useAddLiquidity = () => {
+  const { errorNotification, successNotification } = useNotification();
+  const { wallet } = useWallet();
+
+  const mutation = useMutation({
+    mutationFn: async (liquidityToAdd: number) => {
+      if (!wallet) {
+        throw new Error("Wallet must be connected to add liquidity");
+      }
+
+      const liquidityPool = new LiquidityPool(
+        liquidityPoolContractAddress,
+        wallet
+      );
+      // TODO: make liquidityToAdd a BN
+      const response = await liquidityPool.functions
+        .deposit()
+        .callParams({ forward: [liquidityToAdd * 10 ** 9, usdsAssetId] })
+        .call();
+
+      await response.waitForResult();
+    },
+    onError: (error) => {
+      console.error(error.message);
+      errorNotification(error.message);
+    },
+    onSuccess: () => {
+      successNotification("Successfully added liquidity.");
+    },
+  });
+
+  return mutation;
+};
--- app/fuels.config.ts
+++ app/fuels.config.ts
@@ -1,18 +1,82 @@
-import { createConfig } from 'fuels';
-import dotenv from 'dotenv';
+import {
+  ContractDeployOptions,
+  createConfig,
+  DeployedData,
+  FuelsConfig,
+} from "fuels";
+import dotenv from "dotenv";
+import { Provider, Wallet, defaultConsensusKey } from "fuels";
 
-import { providerUrl } from './lib';
+import { LiquidityPool } from "./frontend/src/types";
+import { providerUrl } from "./lib";
 
 dotenv.config({
-	path: ['.env.local', '.env'],
+  path: [".env.local", ".env"],
 });
 
 // If your node is running on a port other than 4000, you can set it here
 const fuelCorePort = +(process.env.VITE_FUEL_NODE_PORT as string) || 4000;
 
 export default createConfig({
-	workspace: '../contracts', // Path to your Sway workspace
-	output: './frontend/src/types', // Where your generated types will be saved
-	fuelCorePort,
-	providerUrl,
+  workspace: "../contracts", // Path to your Sway workspace
+  output: "./frontend/src/types", // Where your generated types will be saved
+  fuelCorePort,
+  providerUrl,
+  deployConfig: async (options: ContractDeployOptions) => {
+    const { contracts, contractName } = options;
+
+    if (contractName === "liquidityPool") {
+      const usdsContract = contracts.find((contract) => {
+        return contract.name === "usds";
+      });
+
+      if (!usdsContract) {
+        throw new Error("USDS contract not deployed");
+      }
+
+      let depositAssetId = { bits: usdsContract.contractId };
+
+      console.log(`depositAssetId`, depositAssetId);
+      return {
+        configurableConstants: { DEPOSIT_ASSET_ID: depositAssetId },
+      };
+    }
+
+    return {};
+  },
+  onDeploy: async (config: FuelsConfig, data: DeployedData) => {
+    const contracts = data.contracts;
+
+    const liquidityPoolContract = contracts?.find((contract) => {
+      return contract.name === "liquidityPool";
+    });
+
+    const gameContract = contracts?.find((contract) => {
+      return contract.name === "game";
+    });
+
+    if (!liquidityPoolContract || !gameContract) {
+      throw new Error("Contract not found");
+    }
+
+    const provider = await Provider.create(providerUrl);
+    const privateKey = defaultConsensusKey;
+    const wallet = Wallet.fromPrivateKey(privateKey, provider);
+
+    const liquidityPool = new LiquidityPool(
+      liquidityPoolContract.contractId,
+      wallet
+    );
+
+    const response = await liquidityPool.functions
+      .initialize(
+        {
+          Address: { bits: wallet.address.toString() },
+        },
+        { bits: gameContract.contractId }
+      )
+      .call();
+
+	await response.waitForResult();
+  },
 });
