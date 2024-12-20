338480bcf4abe5fac7f18610d67171708295530b
AurelienFT <aurelien.foucault@epitech.eu>
2024-12-20 02:30:42 +0100

    update game and finish backend script

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,14 +1,36 @@
-import * as dotenv from 'dotenv'
-import { Provider, Wallet } from 'fuels';
-import { Game } from '../../frontend/src/types/contracts/Game'
-const GAME_CONTRACT_ID = "0x639ba1cd8af7a5c028f7c4f826cbe9c91cb2feb14f46d674676eee3232bd2917";
-dotenv.config()
+import * as dotenv from "dotenv";
+import { getRandomB256, Provider, Wallet } from "fuels";
+import { Game } from "./types/contracts/Game";
+
+const GAME_CONTRACT_ID =
+  "0xa27f39765e72e727eaad6ffbca8814b7ba00f47045cd6f42db6ad4e603f0e895";
+dotenv.config();
 
 const provider = await Provider.create(
-    'https://testnet.fuel.network/v1/graphql'
-  );
+  "https://testnet.fuel.network/v1/graphql"
+);
 
 const wallet = Wallet.fromPrivateKey(process.env.PRIVATE_KEY!, provider);
 while (true) {
-    
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const game = new Game(GAME_CONTRACT_ID, wallet);
+  const res = await game.functions.is_mature().dryRun();
+  if (!res.value) {
+    console.log("Game is not mature yet");
+    continue;
+  }
+  console.log("Game is mature");
+  try {
+    const { waitForResult } = await game.functions
+    .request_random(getRandomB256())
+    .callParams({ forward: [300, wallet.provider.getBaseAssetId()] })
+    .call();
+  await waitForResult();
+  console.log("Random number requested");
+  } catch (e: unknown) {
+  }
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+  const randomNumber = await game.functions.fulfill_random().call();
+  await randomNumber.waitForResult();
+  console.log("Game fully resolved");
 }
